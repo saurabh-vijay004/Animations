ad3b2b9ad51a45536b6c7f000ac89082f0aa66a3
saurabh-vijay004 <saurabh.vijay@iksulaops.com>
2018-05-23 13:49:17 +0530

    first commit

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -10,6 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var image: UIImageView!
+    
+    var counter = 1
+    var timer = Timer()
+    var isAnimating = false;
+    
+    func animate(){
+        
+        image.image = UIImage(named: "frame_\(counter)_delay-0.1s.gif")
+        
+        counter += 1
+    
+        if counter == 6 {
+            
+            counter = 0
+            
+        }
+        
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        
+        if isAnimating {
+            
+            timer.invalidate()
+            button.setTitle("Start Animation", for: [])
+            
+            isAnimating = false
+            
+        } else{
+            
+            button.setTitle("Stop Animation", for: [])
+            
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
+        
+            isAnimating = true;
+            
+        }
+        
+    }
+    
+    @IBAction func fadeIn(_ sender: Any) {
+    
+        print("fadeIn clicked!")
+        
+        image.alpha = 0
+        
+        UIView.animate(withDuration: 2, animations: {
+        
+            self.image.alpha = 1
+        
+        })
+        
+    }
+    
+    @IBAction func slideIn(_ sender: Any) {
+    
+        print("slideIn clicked!")
+        
+        image.center = CGPoint(x: image.center.x - 500, y: image.center.y)
+        
+        UIView.animate(withDuration: 3, animations: {
+        
+            self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y)
+            
+        })
+        
+    }
+    
+    @IBAction func grow(_ sender: Any) {
+    
+        image.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        
+        UIView.animate(withDuration: 3, animations: {
+            
+            self.image.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+            
+        })
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
